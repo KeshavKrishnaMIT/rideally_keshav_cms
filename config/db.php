5146d33a0012657958cb9d2c17cd7f09d9502aa7
Keshav Krishna Singh <keshavkrishna235@gmail.com>
2026-06-07 01:48:09 +0530

    database config updated

--- config/db.php
+++ config/db.php
@@ -1,8 +1,20 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'cms_db');
+
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'cms_db');
+
+} else {
+
+    define('DB_HOST', 'sql105.infinityfree.com');
+    define('DB_USER', 'if0_42118190');
+    define('DB_PASS', '9mJYR29qQX50o');
+    define('DB_NAME', 'if0_42118190_cms_db');
+
+}
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
